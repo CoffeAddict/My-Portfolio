f89ac6c2e3c8ba2a87e8c16b32cf683662986e99
Andres <andresrodriguezesteban@gmail.com>
2021-04-28 21:32:59 -0300

    added color and layout

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,12 +1,29 @@
 <template>
-  Layout
+    <div class="b-black t-white main-container">
+        <NavBar/>
+        <BodyContent/>
+        <Footer/>
+    </div>
 </template>
 
 <script>
 
+import NavBar from '@/components/NavBar/NavBar'
+import BodyContent from '@/components/BodyContent/BodyContent'
+import Footer from '@/components/Footer/Footer'
+
 export default {
-  name: 'App',
+  name: 'Layout',
   components: {
+    NavBar,
+    BodyContent,
+    Footer
   }
 }
 </script>
+
+<style scoped>
+.main-container {
+    height: 100vh;
+}
+</style>
