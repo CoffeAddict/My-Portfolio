6647a1ad27165cd1029791c26f14712207133cad
Andres Rodriguez <andresrodriguezesteban@gmail.com>
2024-04-29 20:31:12 -0300

    2.0 version - initial files

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './styles/normalize.css'
+import './styles/fonts.css'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Portfolio</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore repellendus
+        explicabo omnis, ullam, voluptas natus aut consequatur recusandae autem dolore corporis ex!
+        Natus atque magnam placeat, ullam amet illo delectus aliquid iste deserunt culpa pariatur
+        consequuntur sequi. <a href="">Temporibus</a> alias reiciendis vitae sint laborum. Eos maiores officia
+        rerum fugit natus ipsa.
+      </p>
+    </>
+  )
+}
+
+export default App
