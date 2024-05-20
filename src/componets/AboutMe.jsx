c73227473b95f7fe42cf5a25ac6632c3d2a95d55
andresrodriguezesteban <andresrodriguezesteban@gmail.com>
2024-05-20 17:27:43 +0000

    02 - webcopy review changes

--- src/componets/AboutMe.jsx
+++ src/componets/AboutMe.jsx
@@ -10,19 +10,19 @@ export const AboutMe = React.forwardRef((props, ref) => {
             <Title text="About Me" elementType="h2"/>
             <div className="text-container">
                 <p>
-                    Hey! I'm Andrés Rodríguez, a front-end developer from Argentina. I've been working on making the Internet a better place since 2018. <br /> I'm working at  <a href="https://www.dosalcubo.com/" target='_blank'>Dos al Cubo</a>, where I'm focused on creating amazing features and enhancing our client's experience.
+                    Hey! I'm Andrés Rodríguez, a front-end developer from Argentina. I've been working on making the Internet a better place since 2018. <br /> Today, I'm at <a href="https://www.dosalcubo.com/" target='_blank'>Dos al Cubo</a>, focused on creating amazing features and enhancing our client's experience.
                 </p>
                 <Icons iconName={'svg-8'}/>
             </div>
             <div className="text-container">
                 <p>
-                    When I'm not working , I'm always eager to explore new frontiers, whether diving into the latest development technologies or scientific topics.
+                    Between pixels and code, I'm always eager to explore new frontiers, whether diving into the latest development technologies or scientific topics.
                 </p>
                 <Icons iconName={'svg-9'}/>
             </div>
             <div className="text-container">
-                    <p>
-                I'm currently pursuing a degree in Video Game Development to expand my skills and knowledge in the field. In my spare time, I like to play video games, which serves as a hobby and a source of inspiration for my studies.
+                <p>
+                    I'm currently pursuing a degree in Video Game Development to expand my skills and knowledge in the field. In my spare time, I like to play video games, which serves as a hobby and a source of inspiration for my studies.
                 </p>
                 <Icons iconName={'svg-10'}/>
             </div>
